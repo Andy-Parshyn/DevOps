f659140ff20ab746bfc90eed79660cbb80dc3ef3
Andy-Parshyn <andriy.parshyn@gmail.com>
2026-04-04 14:03:42 +0200

    fonal touch

--- lesson-5_terraform/outputs.tf
+++ lesson-5_terraform/outputs.tf
@@ -1,3 +1,4 @@
+# S3 backend outputs
 output "s3_bucket_name" {
   description = "Назва S3-бакета для стейтів"
   value       = module.s3_backend.s3_bucket_name
@@ -8,3 +9,24 @@ output "dynamodb_table_name" {
   value       = module.s3_backend.dynamodb_table_name
 }
 
+# VPC outputs
+output "vpc_id" {
+  description = "ID створеного VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "ID публічних підмереж"
+  value       = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "ID приватних підмереж"
+  value       = module.vpc.private_subnet_ids
+}
+
+# ECR outputs
+output "ecr_repository_url" {
+  description = "URL ECR репозиторію"
+  value       = module.ecr.ecr_repository_url
+}
